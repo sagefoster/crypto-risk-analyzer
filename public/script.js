0dd6418d2a68e915435489e9d4e0250eed508578
sagefoster <sagefoster13@gmail.com>
2025-11-21 16:49:18 -0600

    Move price to right of chart, add mobile responsive layout for charts

--- public/script.js
+++ public/script.js
@@ -624,13 +624,13 @@ document.addEventListener('DOMContentLoaded', async () => {
                                         <div class="asset-info-left">
                                             <span class="asset-ticker" id="token${tokenIndex}-ticker"></span>
                                             <span class="asset-name" id="token${tokenIndex}-name"></span>
-                                            <span class="asset-price" id="token${tokenIndex}-price"></span>
                                             <span class="asset-marketcap" id="token${tokenIndex}-marketcap"></span>
                                         </div>
                                         <div class="asset-chart-container">
                                             <div class="chart-labels" id="token${tokenIndex}-chart-labels"></div>
                                             <canvas id="token${tokenIndex}-chart" class="asset-chart"></canvas>
                                         </div>
+                                        <span class="asset-price-chart" id="token${tokenIndex}-price"></span>
                                     </div>
                                 </div>
                 </div>
